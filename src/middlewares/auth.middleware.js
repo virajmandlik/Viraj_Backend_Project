48f136b75f84fbd8a05feaa09dd6f065a2cbd33f
Viraj Mandlik <viraj.mandlik@mitaoe.ac.in>
2024-07-06 13:49:58 +0530

    did login and logout, created the auth middleware

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -27,7 +27,7 @@ export const verifyJWT = asyncHandler(async (req,_,next)=>{
      req.user = user
      next()  
    } catch (error) {
-    throw new ApiError(401,error?.message || "Invalid access token")
+    throw new ApiError(401,error?.message || "Invalid access token...!")
    }  
 
 })
